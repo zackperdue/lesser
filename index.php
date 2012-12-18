1b88f3857d70749d67e8f9f7dfe0a36ec86062c5
Zack Perdue <Zack@Zacharys-MacBook-Pro.local>
2012-12-18 15:40:15 -0500

    First Commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<link rel="stylesheet" href="lesser.php?src=style.less" />
+	<meta charset="UTF-8">
+	<title>Lesser Tester</title>
+</head>
+<body>
+	<p>This is a test for lesser. Inline less -> css compiler.</p>
+</body>
+</html>
